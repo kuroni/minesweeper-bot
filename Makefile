58fa12efaf1bead44fc2bd7a94322a2046559672
Trung Dang <bill081001@gmail.com>
2018-06-18 23:15:41 +0700

    Switched to a lightweight library

--- Makefile
+++ Makefile
@@ -2,10 +2,11 @@ SRCDIR := src
 OBJDIR := obj
 BINDIR := bin
 INCDIR := include
-OBJ := $(patsubst $(SRCDIR)/%.cpp, $(OBJDIR)/%.o, $(wildcard $(SRCDIR)/*.cpp))
+OBJ := $(OBJDIR)/main.o $(OBJDIR)/qdbmp.o
 INC := $(INCDIR)/handle.h $(INCDIR)/solve.h $(INCDIR)/declare.h
 LDFLAGS := -lgdi32
 CXXFLAGS := -std=c++17 -O2 -Wall -I$(INCDIR)
+CFLAGS := -std=c11 -O2 -Wall -I$(INCDIR)
 
 .PHONY: clean
 
@@ -15,8 +16,8 @@ $(BINDIR)/out: $(OBJ)
 $(OBJDIR)/main.o: $(SRCDIR)/main.cpp $(INC)
 	$(CXX) $(CXXFLAGS) -c -o $@ $< $(LDFLAGS)
 
-$(OBJDIR)/EasyBMP.o: $(SRCDIR)/EasyBMP.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $< $(LDFLAGS)
+$(OBJDIR)/qdbmp.o: $(SRCDIR)/qdbmp.c
+	$(CC) $(CFLAGS) -c -o $@ $< $(LDFLAGS)
 
 clean:
 	$(RM) $(OBJ)
--- include/handle.h
+++ include/handle.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "declare.h"
-#include "EasyBMP.h"
+extern "C"
+{
+    #include "qdbmp.h"
+}
 
 namespace NHandle
 {
 HWND hwnd;
 RECT win;
+BMP *bmp;
 
 struct STrie
 {
@@ -34,9 +38,11 @@ int hash(SPixel px)
     return (px.r << 16) | (px.g << 8) | px.b;
 }
 
-SPixel conv(RGBApixel *px)
+SPixel scan_BMP(int x, int y)
 {
-    return SPixel((int)px->Red, (int)px->Green, (int)px->Blue);
+    UCHAR r, g, b;
+    BMP_GetPixelRGB(bmp, x, y, &r, &g, &b);
+    return SPixel(r, g, b);
 }
 
 SPixel scan(int x, int y)
@@ -53,7 +59,7 @@ int read(const SPosition &u, STrie *cur)
     // base case, at the leaf of the determine tree
     if (cur->val != -1)
         return cur->val;
-    
+
     // else, go down into the appropriate child of the current node
     int hsh = hash(scan(u.x + cur->pos.x, u.y + cur->pos.y));
     for (std::pair<int, STrie *> &v : cur->ve)
@@ -63,7 +69,7 @@ int read(const SPosition &u, STrie *cur)
 }
 
 // construct a determine tree for a set of elements (board number, mine number, status face)
-STrie *construct(std::vector<int> ve, BMP &bmp, SPosition st, int h, int w, int dis)
+STrie *construct(std::vector<int> ve, SPosition st, int h, int w, int dis)
 {
     // base case, the number of element is 1
     if (ve.size() == 1)
@@ -77,7 +83,7 @@ STrie *construct(std::vector<int> ve, BMP &bmp, SPosition st, int h, int w, int 
         {
             hsh.clear();
             for (int &v : ve)
-                hsh.push_back(hash(conv(bmp(st.x + dis * v + x, st.y + y))));
+                hsh.push_back(hash(scan_BMP(st.x + dis * v + x, st.y + y)));
             sort(hsh.begin(), hsh.end());
             hsh.resize(std::distance(hsh.begin(), std::unique(hsh.begin(), hsh.end())));
             ans = std::max(ans, std::make_pair((unsigned int)hsh.size(), SPosition(x, y)));
@@ -91,16 +97,16 @@ STrie *construct(std::vector<int> ve, BMP &bmp, SPosition st, int h, int w, int 
     STrie *board = new STrie(-1, ans.second);
     hsh.clear();
     for (int &v : ve)
-        hsh.push_back(hash(conv(bmp(st.x + dis * v + board->pos.x, st.y + board->pos.y))));
+        hsh.push_back(hash(scan_BMP(st.x + dis * v + board->pos.x, st.y + board->pos.y)));
     sort(hsh.begin(), hsh.end());
     hsh.resize(std::distance(hsh.begin(), std::unique(hsh.begin(), hsh.end())));
     for (int &u : hsh)
     {
         std::vector<int> ne;
         for (int &v : ve)
-            if (hash(conv(bmp(st.x + dis * v + board->pos.x, st.y + board->pos.y))) == u)
+            if (hash(scan_BMP(st.x + dis * v + board->pos.x, st.y + board->pos.y)) == u)
                 ne.push_back(v);
-        board->ve.push_back(std::make_pair(u, construct(ne, bmp, st, h, w, dis)));
+        board->ve.push_back(std::make_pair(u, construct(ne, st, h, w, dis)));
         if (board->ve.back().second == nullptr)
             return nullptr;
     }
@@ -110,15 +116,14 @@ STrie *construct(std::vector<int> ve, BMP &bmp, SPosition st, int h, int w, int 
 bool init_skin()
 {
     // construct the original skins (XP skin and 98 skin)
-    BMP bmp;
-    bmp.ReadFromFile("data/winxp.bmp");
-    boardxp = construct({0, 1, 2, 3, 4, 5, 6, 7, 8}, bmp, SPosition(0, 0), 16, 16, 16);
-    minexp = construct({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, bmp, SPosition(0, 33), 11, 21, 12);
-    facexp = construct({0, 2, 3}, bmp, SPosition(0, 55), 26, 26, 27);
-    bmp.ReadFromFile("data/win98.bmp");
-    board98 = construct({0, 1, 2, 3, 4, 5, 6, 7, 8}, bmp, SPosition(0, 0), 16, 16, 16);
-    mine98 = construct({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, bmp, SPosition(0, 33), 11, 21, 12);
-    face98 = construct({0, 2, 3}, bmp, SPosition(0, 55), 26, 26, 27);
+    bmp = BMP_ReadFile("data/winxp.bmp");
+    boardxp = construct({0, 1, 2, 3, 4, 5, 6, 7, 8}, SPosition(0, 0), 16, 16, 16);
+    minexp = construct({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, SPosition(0, 33), 11, 21, 12);
+    facexp = construct({0, 2, 3}, SPosition(0, 55), 26, 26, 27);
+    bmp = BMP_ReadFile("data/win98.bmp");
+    board98 = construct({0, 1, 2, 3, 4, 5, 6, 7, 8}, SPosition(0, 0), 16, 16, 16);
+    mine98 = construct({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, SPosition(0, 33), 11, 21, 12);
+    face98 = construct({0, 2, 3}, SPosition(0, 55), 26, 26, 27);
 
     // find the skin that the current Minesweeper X process uses
     HKEY reg;
@@ -156,13 +161,13 @@ bool init_skin()
 
     // custom skin path lies within the SkinPath key
     RegQueryValueEx(reg, TEXT("SkinPath"), NULL, NULL, (LPBYTE)&val, &len);
-    bmp.ReadFromFile(val);
+    bmp = BMP_ReadFile(val);
     RegCloseKey(reg);
-    if ((board = construct({0, 1, 2, 3, 4, 5, 6, 7, 8}, bmp, SPosition(0, 0), 16, 16, 16)) == nullptr)
+    if ((board = construct({0, 1, 2, 3, 4, 5, 6, 7, 8}, SPosition(0, 0), 16, 16, 16)) == nullptr)
         return false;
-    if ((mine = construct({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, bmp, SPosition(0, 33), 11, 21, 12)) == nullptr)
+    if ((mine = construct({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, SPosition(0, 33), 11, 21, 12)) == nullptr)
         return false;
-    if ((face = construct({0, 2, 3}, bmp, SPosition(0, 55), 26, 26, 27)) == nullptr)
+    if ((face = construct({0, 2, 3}, SPosition(0, 55), 26, 26, 27)) == nullptr)
         return false;
     return true;
 }
@@ -178,8 +183,6 @@ void init_game()
     GetWindowRect(hwnd, &win);
     m = (win.bottom - win.top - 116) / 16;
     n = (win.right - win.left - 30) / 16;
-    min = read(SPosition(win.left + 21, win.top + 63), mine) * 100 
-        + read(SPosition(win.left + 34, win.top + 63), mine) * 10 
-        + read(SPosition(win.left + 47, win.top + 63), mine);
+    min = read(SPosition(win.left + 21, win.top + 63), mine) * 100 + read(SPosition(win.left + 34, win.top + 63), mine) * 10 + read(SPosition(win.left + 47, win.top + 63), mine);
 }
 } // namespace NHandle
